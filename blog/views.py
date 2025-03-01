91aa47e6e871e582c15f11c6c857d00b5b30944e
abproductionit <hovakimyanab@gmail.com>
2025-03-01 14:24:39 +0400

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, PostImage, SiteInfo
 import re
+import base64
 from django.utils.safestring import mark_safe
 from django.http import HttpResponse, Http404
 import mimetypes
@@ -9,9 +10,6 @@ import mimetypes
 def find_image_tags(content):
     """
     Находит все теги $imageN$ в переданном тексте.
-
-    :param content: строка с текстом, содержащая теги $imageN$
-    :return: список найденных тегов
     """
     regex = r"\$image\d+\$"  # Регулярное выражение для поиска $imageN$
     matches = re.findall(regex, content)  # Находим все совпадения
@@ -20,46 +18,38 @@ def find_image_tags(content):
 
 def replace_image_tags(content, images_dict):
     """
-    Заменяет теги $imageN$ в тексте на <img src="URL">
-
-    :param content: строка с текстом
-    :param images_dict: словарь {"$image1$": "URL1", "$image2$": "URL2"}
-    :return: текст с замененными изображениями
+    Заменяет теги $imageN$ в тексте на <img src="data:image/png;base64,..." >
     """
 
     def replacer(match):
         tag = match.group(0)  # Найденный $imageN$
-
-        return f'<p> <img src="data:image/png;base64,{images_dict.get(tag)}" alt="\n🖼\n"> </p>'  # Если нет URL, подставляем #
+        img_base64 = images_dict.get(tag, None)
+        if img_base64:
+            return f'<p> <img src="data:image/png;base64,{img_base64}" alt="\n🖼\n"> </p>'
+        return tag  # Если изображения нет, оставляем как есть
 
     regex = r"\$image\d+\$"  # Регулярное выражение для поиска $imageN$
     return mark_safe(re.sub(regex, replacer, content))
 
 
-def index(request):
-    site_name_info=SiteInfo.objects.all().last()
-    if site_name_info:
-        title = site_name_info.title
-        description = site_name_info.description
-    else:
-        title, description  = "DjangoBlog", "Simple blog project on Django"
-    posts = Post.objects.filter(posted=True)  # выводим только опубликованные посты
-    return render(request, 'index.html', {'posts': posts, 'site_name': title, 'description': description})
-
 def post_detail(request, pk):
     ret_post_img_dict = {}
     post = get_object_or_404(Post, pk=pk)
-    tags = find_image_tags(post.content)
-    p_img = PostImage.objects.filter(tag__in=tags)
-    for el in p_img:
-        ret_post_img_dict[el.tag] = el.image
-    print(ret_post_img_dict)
 
+    # Находим все теги вида $imageN$
+    tags = find_image_tags(post.content)
+
+    # Получаем изображения из базы
+    p_img = PostImage.objects.filter(tag__in=tags)
+
+    # Кодируем в base64 и добавляем в словарь
+    for el in p_img:
+        ret_post_img_dict[el.tag] = base64.b64encode(el.image).decode('utf-8')
+
+    # Заменяем теги $imageN$ на изображения
     new_content = replace_image_tags(post.content, ret_post_img_dict)
-    print(new_content)
 
     return render(request, 'post.html', {'post': post, 'content': new_content})
-
 
 def image_view(request, post_id):
     """
@@ -70,6 +60,6 @@ def image_view(request, post_id):
     if not post.main_image:
         raise Http404("Изображение не найдено")
 
-    # Определяем MIME-тип (по умолчанию image/png)
-    content_type, _ = mimetypes.guess_type("image.png")  # Можно менять на "image/jpeg", если нужны JPG
+    # Определяем MIME-тип (если известен)
+    content_type, _ = mimetypes.guess_type("image.png")  # Меняйте на image/jpeg, если используете JPG
     return HttpResponse(post.main_image, content_type=content_type)
